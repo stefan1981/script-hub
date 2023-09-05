c7a0b484e93e7e616e73cf07091b805cb849763d
Stefan Beck <beck@publicplan.de>
2023-09-05 09:39:41 +0200

    add kubernetes helper script

---
+++ k8/show-ressources-from-current-namespace.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "${RED}$(kubens -c)${NC}\n"
+
+status () {
+  if [[ $(kubectl get $1 2>&1) != *"No resources"*  ]]; then
+    printf "\n${GREEN}--- $1 ${NC}\n"
+    kubectl get $1
+  fi
+}
+
+status pods
+status deployments
+status statefulsets
+status cronjob
+status job
+status service
+status endpoints
+status configmaps
+status secrets
+status persistentvolumeclaim
+status persistentvolume
+
