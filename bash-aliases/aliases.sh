0a569fbe624dbb6663fcf77d1d0914f9540f5f3c
Stefan Beck <beck@publicplan.de>
2023-09-15 17:32:22 +0200

    Changes in config Fr 15. Sep 17:32:22 CEST 2023

--- bash-aliases/aliases.sh
+++ bash-aliases/aliases.sh
@@ -3,5 +3,26 @@
 
 # Kubernetes
 alias k='kubectl "--context=${KUBECTL_CONTEXT:-$(kubectl config current-context)}"'
-alias watch='watch -n 1 -c '
+alias xx-watch='watch -n 1 -c '
 alias kshow='watch -n 1 /bin/bash ~/script-hub/k8/show-ressources-from-current-namespace.sh'
+
+
+# cheat-cheat
+xx-cheat() {
+    clear; echo "----------------------------------------------"
+    curl cht.sh/$1 | less
+}
+
+# push changes in config
+xx-alias-push() {
+  cd ~/script-hub
+  git add .
+  git commit -m "Changes in config $(date)"
+  gi push
+}
+
+alias xx-zshrc='nvim ~/.zshrc'
+alias xx-zshrc-source='source ~/.zshrc'
+alias xx-alias='nvim ~/script-hub/bash-aliases/aliases.sh'
+
+
--- k8/show-ressources-from-current-namespace.sh
+++ k8/show-ressources-from-current-namespace.sh
@@ -9,19 +9,21 @@ printf "${RED}$(kubens -c)${NC}\n"
 status () {
   if [[ $(kubectl get $1 2>&1) != *"No resources"*  ]]; then
     printf "\n${GREEN}--- $1 ${NC}\n"
-    kubectl get $1
+    kubectl get $1 -o wide
   fi
 }
 
 status pods
 status deployments
 status statefulsets
+status replicasets
 status cronjob
 status job
 status service
-status endpoints
+#status endpoints
 status configmaps
 status secrets
 status persistentvolumeclaim
-status persistentvolume
+#status persistentvolume
+#status serviceaccount
 
